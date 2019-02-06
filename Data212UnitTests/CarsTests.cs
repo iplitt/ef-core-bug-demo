a67b6fe2d1143633285726c009ccbee4b177e041
Irwin Plitt <iplitt@alum.mit.edu>
2019-02-05 16:29:20 -0800

    First case demonstrating discrepancy between NuGet versions.

--- Data212UnitTests/CarsTests.cs
+++ Data212UnitTests/CarsTests.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Data212;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Data212UnitTests
@@ -11,47 +12,75 @@ namespace Data212UnitTests
     {
         private ICars _testObject;
         private DemoContext _inMemoryDemoContext;
-        private string _carId;
+        private string _carId1;
+        private string _carId2;
         private string _driverId1;
         private string _driverId2;
+        private string _driverId3;
+        private List<Car> _cars;
+        private List<CarDriver> _carDrivers;
+        private List<Driver> _drivers;
 
         [SetUp]
         public void SetUp()
         {
-            _inMemoryDemoContext =
-                new InMemoryDemoContext() { ProviderSpecificCommands = new InMemoryProviderSpecificCommands() };
-            _inMemoryDemoContext.Database.EnsureDeleted();
+            _carId1 = "C1";
+            _carId2 = "C2";
+            _driverId1 = "D1";
+            _driverId2 = "D2";
+            _driverId3 = "D3";
+            _cars = new List<Car>
+            {
+                new TestCarBuilder().WithCarId(_carId1).Build()
+            };
+            _drivers = new List<Driver>
+            {
+                new TestDriverBuilder().WithDriverId(_driverId1).Build()
+            };
+            _carDrivers = new List<CarDriver>
+            {
+                new TestCarDriverBuilder().WithCarId(_carId1).WithDriverId(_driverId1).Build() 
+            };
 
-            _carId = "43578";
-            _driverId1 = "1";
-            _driverId2 = "2";
-            var car = new TestCarBuilder().WithCarId(_carId).Build();
-            var driver1 = new TestDriverBuilder().WithDriverId(_driverId1).Build();
-            var driver2 = new TestDriverBuilder().WithDriverId(_driverId2).Build();
-            var carDrivers = new List<CarDriver> { new CarDriver { CarId = _carId, DriverId = _driverId1 } };
-            _inMemoryDemoContext.Cars.Add(car);
-            _inMemoryDemoContext.Drivers.Add(driver1);
-            _inMemoryDemoContext.Drivers.Add(driver2);
-            _inMemoryDemoContext.CarDrivers.AddRange(carDrivers);
-            _inMemoryDemoContext.SaveChanges();
+            Initialize();
 
             _testObject = new Cars(_inMemoryDemoContext);
         }
 
-
         [Test]
-        public async Task Upsert_AddsNewCarDrivers()
+        public async Task ExposesChildren()
         {
-            var existingCar = _inMemoryDemoContext.Cars.Single();
+            var newDrivers = new List<Driver>
+            {
+                new TestDriverBuilder().WithDriverId(_driverId2).Build(),
+                new TestDriverBuilder().WithDriverId(_driverId3).Build()
+            };
+            _drivers.AddRange(newDrivers);
 
-            var newCarDriver = new CarDriver {CarId = _carId, DriverId = _driverId2};
-            existingCar.CarDrivers.Add(newCarDriver);
-            await _inMemoryDemoContext.SaveChangesAsync();
+            _cars.Add(new TestCarBuilder().WithCarId(_carId2).Build());
 
-            await _testObject.Upsert(existingCar);
+            _carDrivers.AddRange(new List<CarDriver>
+            {
+                new TestCarDriverBuilder().WithCarId(_carId1).WithDriverId(_driverId2).Build(),
+                new TestCarDriverBuilder().WithCarId("C2").WithDriverId(_driverId3).Build()
+            });
 
-            var car = _inMemoryDemoContext.Cars.Single();
-            Assert.That(car.CarDrivers.Any(x => x.DriverId == _driverId2), Is.True);
+            _inMemoryDemoContext.SaveChanges();
+
+            var actual = await _testObject.Get(new List<string> {_carId1});
+            Console.WriteLine(JsonConvert.SerializeObject(actual));
+
+        }
+
+        private void Initialize()
+        {
+            _inMemoryDemoContext = new InMemoryDemoContext() { ProviderSpecificCommands = new InMemoryProviderSpecificCommands() };
+            _inMemoryDemoContext.Database.EnsureDeleted();
+
+            _inMemoryDemoContext.Cars.AddRange(_cars);
+            _inMemoryDemoContext.Drivers.AddRange(_drivers);
+            _inMemoryDemoContext.CarDrivers.AddRange(_carDrivers);
+            _inMemoryDemoContext.SaveChanges();
         }
 
 
